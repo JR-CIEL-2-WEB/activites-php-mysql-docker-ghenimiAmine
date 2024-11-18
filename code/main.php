abe1ce4ab0f5fd8bd264eac8bcbb62776f0c7e24
Amine Ghenimi <144660090+ghenimiAmine@users.noreply.github.com>
2024-11-18 16:42:45 +0000

    médiane

--- code/main.php
+++ code/main.php
@@ -2,5 +2,8 @@
 include 'statistique.php';
 $tab = [15, 12, 9]; 
 $moyenneNotes = moyenne($tab);
-echo "La moyenne des notes est : " . $moyenneNotes;
+echo "La moyenne des notes est : " . $moyenneNotes . "<br>";
+
+mediane([2007, 2002, 2003, 2005, 2020, 2016, 2011]);
+mediane([1500, 4500, 2200, 1500, 3300, 1800,1700, 2000,4000]);
 ?>
--- code/statistique.php
+++ code/statistique.php
@@ -7,4 +7,15 @@ function moyenne($tab) {
     $somme = array_sum($tab);
     return $somme / count($tab);
 }
+
+function mediane(array $listes) {
+    sort($listes);
+    $nb_elements = count($listes);
+    if ($nb_elements % 2 == 0) {
+        $milieu = ($listes[$nb_elements / 2 - 1] + $listes[$nb_elements / 2]) / 2;
+    } else {
+        $milieu = $listes[floor($nb_elements / 2)];
+    }
+    echo "La médiane est " . $milieu . "<br>";
+}
 ?>
